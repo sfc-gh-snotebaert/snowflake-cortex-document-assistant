e590576ee6af9c6903560adc54a8426189e0817f
Doneyli De Jesus <doneyli@gmail.com>
2025-03-19 13:04:30 -0400

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -5,25 +5,6 @@ ALTER GIT REPOSITORY GITHUB_REPO_AI_CONTRACT_INTELLIGENCE FETCH;
 
 -- Create Streamlit app with method based on status of Behavior Change Bundle 2025_01
 BEGIN
-  -- Check if 2025 Bundle is enabled (multi-page streamlit apps)
-  LET status_2025_01 STRING := (SELECT SYSTEM$BEHAVIOR_CHANGE_BUNDLE_STATUS('2025_01'));
-  -- Log result
-  SYSTEM$LOG_INFO('Bundle 2025_01 is ' || :status_2025_01);
-  
-  -- Check if the bundle is ENABLED and execute appropriate commands
-  IF (status_2025_01 = 'ENABLED') THEN
-
-    -- Create Streamlit with multifile editing
-    CREATE STREAMLIT CORTEX_AI_CONTRACT_CHAT_APP
-      FROM @DOC_EXTRACTION_DB.PUBLIC.GITHUB_REPO_AI_CONTRACT_INTELLIGENCE/branches/main/app/
-      MAIN_FILE = 'streamlit_app.py'
-      QUERY_WAREHOUSE = COMPUTE_WH
-      TITLE = 'Cortex AI Contracts Chat App'
-      COMMENT = 'Demo Streamlit frontend for Contract Intelligence';
-
-    RETURN 'Bundle 2025_01 is ENABLED. Created Streamlit app with multi-file editing.';
-  ELSE
-    -- Check if the bundle is DISABLED and execute appropriate commands
       -- Create stage for the Streamlit App
       CREATE OR REPLACE STAGE STREAMLIT_APP
         DIRECTORY = (ENABLE = TRUE)
@@ -42,7 +23,4 @@ BEGIN
           QUERY_WAREHOUSE = COMPUTE_WH
           TITLE = 'Cortex AI Contracts Chat App'
           COMMENT = 'Demo Streamlit frontend for Contract Intelligence';
-
-      RETURN 'Bundle 2025_01 is DISABLED. Created Streamlit app with single file editing.';
-  END IF;
 END;
